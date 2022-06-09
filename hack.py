3f1e3802836ab6ed0a8a8ce3b65bf97a0b244466
DimitryRukhadze <lokiinflames@gmail.com>
2022-06-09 12:23:08 +0300

    Update hack.py

--- hack.py
+++ hack.py
@@ -82,36 +82,37 @@ def get_subject_names_for_kid(kid):
     return subject_names
 
 
-logging.basicConfig(format=f'%(levelname)s %(message)s')
+if __name__ == '__main__':
+    logging.basicConfig(format=f'%(levelname)s %(message)s')
 
-kid_name = input('Введите имя ученика: ')
-similar_names = get_similar_names(kid_name)
+    kid_name = input('Введите имя ученика: ')
+    similar_names = get_similar_names(kid_name)
 
-try:
-    kid_object = Schoolkid.objects.get(full_name__contains=kid_name)
-except django.core.exceptions.MultipleObjectsReturned:
-    logging.critical('Много похожих имён. Ввведите одно из них')
-    print(similar_names)
-    exit()
-except django.core.exceptions.ObjectDoesNotExist:
-    logging.critical('Ученика с таким именем нет. Проверьте правильность написания')
-    exit()
+    try:
+        kid_object = Schoolkid.objects.get(full_name__contains=kid_name)
+    except django.core.exceptions.MultipleObjectsReturned:
+        logging.critical('Много похожих имён. Ввведите одно из них')
+        print(similar_names)
+        exit()
+    except django.core.exceptions.ObjectDoesNotExist:
+        logging.critical('Ученика с таким именем нет. Проверьте правильность написания')
+        exit()
 
-kid_subject_names = get_subject_names_for_kid(kid_object)
+    kid_subject_names = get_subject_names_for_kid(kid_object)
 
-try:
-    subject_name = input('Введите название предмета: ')
-    subject_object = Subject.objects.get(
-        title=subject_name,
-        year_of_study=kid_object.year_of_study
-    )
-except django.core.exceptions.ObjectDoesNotExist:
-    logging.critical('Такого урока нет у этого ученика')
-    print('Вот правильные названия уроков:')
-    print(kid_subject_names)
-    exit()
+    try:
+        subject_name = input('Введите название предмета: ')
+        subject_object = Subject.objects.get(
+            title=subject_name,
+            year_of_study=kid_object.year_of_study
+        )
+    except django.core.exceptions.ObjectDoesNotExist:
+        logging.critical('Такого урока нет у этого ученика')
+        print('Вот правильные названия уроков:')
+        print(kid_subject_names)
+        exit()
 
-fix_marks(kid_object)
-remove_chastisements(kid_object)
-create_commendation(kid_object, subject_name)
-print(f'Записи для {kid_name} исправлены. Похвала по {subject_name} добавлена')
+    fix_marks(kid_object)
+    remove_chastisements(kid_object)
+    create_commendation(kid_object, subject_name)
+    print(f'Записи для {kid_name} исправлены. Похвала по {subject_name} добавлена')
